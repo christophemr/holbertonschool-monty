6cc73d9abb74c9af85b2dc18d728e86fbbfd6a00
christophemr <christophemr59@gmail.com>
2024-01-02 12:57:08 -0800

    function that executes instruction based on the opcode

---
+++ selection.c
@@ -0,0 +1,37 @@
+#include "monty.h"
+
+/**
+ * run_instruction - executes instruction based on the opcode
+ * @stack: double pointer to the stack
+ * @line_number: line number in the monty file
+ * @opcode: opcode extracted
+ */
+
+void run_instruction(stack_t **stack, unsigned int line_number, char *opcode)
+{
+	static const instruction_t opc[] = {
+		{"push", push}
+		{"pall", pall}
+		{"pint", pint}
+		{"pop", pop}
+		{"swap", swap}
+		{"add", add}
+		{"nop", nop}
+		{NULL, NULL}};
+
+	int index;
+
+	for (index = 0; opc[index].opcode != NULL; index++)
+	{
+		if (strcmp(opcode, opc[index].opcode) == 0)
+		{
+			opc[index].f(stack, line_number);
+			return;
+		}
+	}
+
+	fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
+	cleanup(*stack);
+	exit(EXIT_FAILURE);
+
+}
