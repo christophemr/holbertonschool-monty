9c9492bdfdc09c53256a0a926af6ba4ac9b9f645
christophemr <christophemr59@gmail.com>
2024-01-03 02:22:33 -0800

    opcode that swaps the top two elements of the stack

---
+++ swap.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+
+/**
+ * swap - swaps the top two element of the stack
+ * @stack: stack
+ * @line_number: line number of the file
+ */
+
+void swap(stack_t **stack, unsigned int line_number)
+{
+	int temp;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		cleanup(*stack);
+		exit(EXIT_FAILURE);
+	}
+	temp = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = temp;
+}
