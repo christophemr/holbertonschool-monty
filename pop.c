9798cfbcc8d1e6686d5d6e0b59fd46375c7b2483
christophemr <christophemr59@gmail.com>
2024-01-04 21:12:32 +0100

    added a check for an empty stack before attempting to pop

--- pop.c
+++ pop.c
@@ -8,14 +8,18 @@
 
 void pop(stack_t **stack, unsigned int line_number)
 {
-	stack_t *temp = *stack;
+	stack_t *temp;
 
-	if (temp == NULL)
+	/* added a check for an empty stack before attempting to pop */
+	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		cleanup(*stack);
 		exit(EXIT_FAILURE);
 	}
+
+	temp = *stack;
+
 	if (temp->next == NULL)
 	{
 		free(*stack);
