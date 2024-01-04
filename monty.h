f02242f5fcf47bcc6eb72c0037161b73a82ae6d3
christophemr <christophemr59@gmail.com>
2024-01-04 21:08:34 +0100

    parameter added to the push.c prototype

--- monty.h
+++ monty.h
@@ -39,7 +39,8 @@ typedef struct instruction_s
 
 void run_instruction(stack_t **stack, unsigned int line_number, char *opcode);
 void cleanup(stack_t *stack);
-void push(stack_t **stack, unsigned int line_number);
+void push(stack_t **stack, unsigned int line_number, int n);
+/* int n added to the push opc */
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
