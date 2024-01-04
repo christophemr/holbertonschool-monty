f360045d8fdedd8fff1c50c3bdabf2fb072d76d6
christophemr <christophemr59@gmail.com>
2024-01-04 14:39:31 +0100

    push.c fix

--- push.c
+++ push.c
@@ -7,13 +7,6 @@
  */
 void push(stack_t **stack, unsigned int line_number)
 {
-	int value = atoi(line_number);
-	if (value == 0 && line_number[0] != '\0')
-	{
-		fprintf(stderr, "L%d: usage: push integer\n", line_number);
-		cleanup(*stack);
-		exit(EXIT_FAILURE);
-	}
 
 	stack_t *new_node = malloc(sizeof(stack_t));
 
@@ -22,7 +15,7 @@ void push(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%d: Error: malloc failed\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	new_node->n = value;
+	new_node->n = line_numbe;
 	new_node->next = *stack;
 	new_node->prev = NULL;
 
