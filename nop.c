ecf6b117f57a535433fefc8a2cfd0d9fa622000a
christophemr <christophemr59@gmail.com>
2024-01-04 15:24:29 +0100

    parameters removal

--- nop.c
+++ nop.c
@@ -3,11 +3,8 @@
 /**
  * nop - doesn't do anything
  * @stack: unused
- * @line_number: unused
- * Return: void
  */
-void nop(stack_t **stack, unsigned int line_number)
+void nop(stack_t **stack)
 {
 	(void) stack;
-	(void) line_number;
 }
